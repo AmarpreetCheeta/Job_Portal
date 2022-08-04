6dc3baaf0116209ce3218d9b03efe0ec5750d6dc
Amarpreet Cheeta <cheetaamar2001@gmail.com>
2022-08-04 21:46:52 +0530

    first commit

---
+++ job_portal/app/static/app/js/searchjobs.js
@@ -0,0 +1,24 @@
+
+
+// const search = () => {
+//     const searchbox = document.getElementById("search-item").value.toUpperCase();
+//     const storeitems = document.getElementById("jobs-list")
+//     const jobs = document.querySelectorAll(".jobs")
+//     const jname = document.querySelectorAll("._types")
+
+//     for (var i = 0; i < jname.length; i++) {
+//         let match = jobs[i].querySelectorAll('._types')[0];
+
+//         if (match) {
+//             let textvalue = match.textContent || match.innerHTML
+
+//             if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+//                 jobs[i].style.display = "";
+//             }
+//             else {
+//                 jobs[i].style.display = "none";
+//                 document.getElementById("_no_").style.display = "block"
+//             }
+//         }
+//     }
+// }
